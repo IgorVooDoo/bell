c23695c55a518a60fbecb969eee52a05fe8de16b
IgorVooDoo <igorvoodoo@gmail.com>
2018-12-05 19:11:33 +0400

    Реализация схемы БД (schema.sql). Добавлены индексы. Проставлена обязательность полей.

--- schema.sql
+++ schema.sql
@@ -18,6 +18,8 @@ CREATE TABLE IF NOT EXISTS Document (
   CONSTRAINT FK__doc_type FOREIGN KEY (type) REFERENCES doc_type (id)
 ) COMMENT = 'Таблица документов работников';
 
+create index Ix_number_doc on Document(doc_number);
+
 /*Country_type - справочная таблица с перечнем стран*/
 CREATE TABLE IF NOT EXISTS Country_type (
     id      INT NOT NULL AUTO_INCREMENT COMMENT 'Уникальный идентификатор',
@@ -26,43 +28,49 @@ CREATE TABLE IF NOT EXISTS Country_type (
     PRIMARY KEY (id)
 ) COMMENT = 'Cправочная таблица с перечнем стран';
 
+
 /*Organization - таблица организаций*/
 CREATE TABLE IF NOT EXISTS Organization (
   id INT NOT NULL AUTO_INCREMENT COMMENT 'Уникальный идентификатор',
   name VARCHAR(20) NOT NULL COMMENT 'Краткое наименование организации',
   full_name VARCHAR(45) NOT NULL COMMENT 'Полное наименование организации',
   adress VARCHAR(100) NOT NULL COMMENT 'Юридический адрес',
-  phone VARCHAR(20) NOT NULL COMMENT 'Номер телефона',
+  phone VARCHAR(20) COMMENT 'Номер телефона',
   inn BIGINT NOT NULL COMMENT 'ИНН',
   kpp BIGINT NOT NULL COMMENT 'КПП',
-  is_active BOOLEAN NOT NULL COMMENT 'Актуальность',
+  is_active BOOLEAN COMMENT 'Актуальность',
   PRIMARY KEY (id)
 ) COMMENT = 'Таблица организаций';
+
+create index Ix_name_org on Organization(name);
+create index Ix_inn_org on Organization(inn);
 
 /*Office - офисы организаций*/
 CREATE TABLE IF NOT EXISTS Office (
   id INT NOT NULL AUTO_INCREMENT COMMENT 'Уникальный идентификатор',
   name VARCHAR(45) NOT NULL COMMENT 'Наименование офиса',
   adress VARCHAR(100) NOT NULL COMMENT 'Адрес расположения офиса',
-  phone VARCHAR(45) NOT NULL COMMENT 'Номер телефона в данном офисе',
-  is_active BOOLEAN NOT NULL COMMENT 'Актуальность',
+  phone VARCHAR(45) COMMENT 'Номер телефона в данном офисе',
+  is_active BOOLEAN COMMENT 'Актуальность',
   org_id INT NOT NULL COMMENT 'Ссылка на организацию, к которой принадлежит офис',
   PRIMARY KEY (id),
   KEY office_organization_fk (org_id),
   CONSTRAINT office_organization_fk FOREIGN KEY (org_id) REFERENCES organization (id)
 ) COMMENT = 'Офисы организации';
 
+create index Ix_name_ofc on Office(name);
+
 /*Employee - таблица сотрудников*/
 CREATE TABLE IF NOT EXISTS Employee (
   id INT NOT NULL AUTO_INCREMENT COMMENT 'Уникальный идентификатор',
   first_name VARCHAR(45) NOT NULL COMMENT 'Имя',
-  last_name VARCHAR(45) NOT NULL COMMENT 'Фамилия',
-  middle_name VARCHAR(45) NOT NULL COMMENT 'Отчество',
+  last_name VARCHAR(45) COMMENT 'Фамилия',
+  middle_name VARCHAR(45) COMMENT 'Отчество',
   position VARCHAR(45) NOT NULL COMMENT 'Должность',
-  doc_id INT NOT NULL COMMENT 'Ссылка на id документа работника в таблице Document',
-  citizenship_id INT NOT NULL COMMENT 'Ссылка на id страны в таблице Country_type',
-  is_identified INT NOT NULL COMMENT '',
-  office_id INT NOT NULL COMMENT 'Ссылка на id офиса в таблице Office',
+  doc_id INT COMMENT 'Ссылка на id документа работника в таблице Document',
+  citizenship_id INT COMMENT 'Ссылка на id страны в таблице Country_type',
+  is_identified INT COMMENT '',
+  office_id INT COMMENT 'Ссылка на id офиса в таблице Office',
   PRIMARY KEY (id),
   KEY employee_document_fk (doc_id),
   CONSTRAINT employee_document_fk FOREIGN KEY (doc_id) REFERENCES document (id),
@@ -71,5 +79,7 @@ CREATE TABLE IF NOT EXISTS Employee (
   KEY employee_country_type_fk (citizenship_id),
   CONSTRAINT employee_country_type_fk FOREIGN KEY (citizenship_id) REFERENCES country_type (id)
 ) COMMENT = 'Таблица сотрудников';
+
+create index Ix_name_emp on Employee(last_name);
 
 
